08665e7603627de2b6134d76763212c3debbf0cc
Romerogon <gonzalog.romero@telefonica.com>
2025-03-14 21:00:25 -0300

    se agrega alerta de error si campo vacio y codigo

--- app.js
+++ app.js
@@ -1,61 +1,58 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+let grupoAmigos = [];
 let listaAmigosSorteados = [];
+
 function listarAmigos(){
     // Selecciona el contenedor principal donde se mostrarán los amigos.
     let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = ''; // Limpia el contenido actual del contenedor.
 
     // Itera sobre el arreglo listaAmigos y crea elementos <li> para cada amigo.
-    let currentUl = document.createElement('ul');
-    listaAmigos.appendChild(currentUl);
-    for (let i = 0; i < listaAmigos.length; i++) {
+    let amigos = document.createElement('ul');
+    listaAmigos.appendChild(amigos);
+    for (let i = 0; i < grupoAmigos.length; i++) {
         
         let li = document.createElement('li'); // Crea un nuevo elemento <li>.
-        li.textContent = `${i + 1}: ${listaAmigos[i]}`; // Asigna el texto al elemento <li>.
-        currentUl.appendChild(li); // Agrega el elemento <li> al elemento <ul> actual.
+        li.textContent = `${i + 1}: ${grupoAmigos[i]}`; // Asigna el texto al elemento <li>.
+        amigos.appendChild(li); // Agrega el elemento <li> al elemento <ul> actual.
     }
 
     // Muestra el total de amigos.
     let totalAmigos = document.createElement('p');
-    totalAmigos.textContent = `Total de amigos: ${listaAmigos.length}`;
-    container.appendChild(totalAmigos);
+    totalAmigos.textContent = `Total de amigos: ${grupoAmigos.length}`;
+    listaAmigos.appendChild(totalAmigos);
 }
 function agregarAmigo(){
     let nombre = document.getElementById('amigo').value;
-    let numero = 0;
+   
     if(nombre === ''){
-        asignarTextoElemento('listaAmigos','Ingresa un nombre valido por favor  <br>' + listaAmigos + '<br>Total de amigos: ' + listaAmigos.length);
+        alert("Por favor ingresar un nombre valido.");         
     } else {
-        listaAmigos.push(nombre);
-        //numero ++;
-        //asignarTextoElemento('listaAmigos', listaAmigos + '<br>Total de amigos: ' + listaAmigos.length);
+        grupoAmigos.push(nombre);
         listarAmigos();
+        amigo.value = '';
     }
 
 }
-
-
-
 function asignarTextoElemento(id, texto) {
     let idElementoHTML = document.getElementById(id); // Selecciona el elemento HTML basado en el selector proporcionado.
     idElementoHTML.innerHTML = texto; // Asigna el texto proporcionado al contenido HTML del elemento.
     return; // Finaliza la función.
 }
 function sortearAmigo(){
-    let amigoSorteado = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
-    if(listaAmigos.length === 0){
-        asignarTextoElemento('resultado','No hay amigos en la lista');
+    let amigoSorteado = grupoAmigos[Math.floor(Math.random() * grupoAmigos.length)];
+    if(grupoAmigos.length === 0){
+        alert("Por favor ingresar al menos un nombre.");
+        
         return;
     }
-    if(listaAmigos.length %2 !== 0){
-        asignarTextoElemento('resultado','No se puede sortear, la cantidad de amigos debe ser par');
-        return;
-    }
-    if(listaAmigosSorteados.includes(amigoSorteado) )
-        return sortearAmigo();
+   
+    
     else{
     listaAmigosSorteados.push(amigoSorteado);
-    asignarTextoElemento('resultado','El amigo Secreto de xx es: ' + amigoSorteado);
+    grupoAmigos=[];
+    asignarTextoElemento('resultado','El amigo Secreto sorteado es: ' + amigoSorteado);
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''
     }
 }
